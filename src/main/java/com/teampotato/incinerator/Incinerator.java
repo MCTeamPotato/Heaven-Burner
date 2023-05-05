fb85a3a917a69b5356cbc768780c70ba0ba0c6ae
Kasualix <670067575@qq.com>
2023-05-05 17:19:30 +0800

    init 1.18.2

--- src/main/java/com/teampotato/incinerator/Incinerator.java
+++ src/main/java/com/teampotato/incinerator/Incinerator.java
@@ -2,17 +2,17 @@ package com.teampotato.incinerator;
 
 import L_Ender.cataclysm.client.render.CMRenderTypes;
 import L_Ender.cataclysm.init.ModItems;
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
-import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.ItemRenderer;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Matrix3f;
+import com.mojang.math.Matrix4f;
+import com.mojang.math.Vector3f;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.vector.Matrix3f;
-import net.minecraft.util.math.vector.Matrix4f;
-import net.minecraft.util.math.vector.Vector3f;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(Incinerator.ID)
@@ -20,7 +20,7 @@ public class Incinerator {
     public static final String ID = "incinerator";
     private static final ResourceLocation FLAME = new ResourceLocation("cataclysm:textures/entity/soul_flame_strike_sigil.png");
 
-    private static void drawVertex(Matrix4f matrix4f, Matrix3f matrix3f, IVertexBuilder builder, int pX, int pZ, float pU, float pV) {
+    private static void drawVertex(Matrix4f matrix4f, Matrix3f matrix3f, VertexConsumer builder, int pX, int pZ, float pU, float pV) {
         builder
                 .vertex(matrix4f, pX, 0, pZ)
                 .color(255, 255, 255, 255)
@@ -31,21 +31,21 @@ public class Incinerator {
                 .endVertex();
     }
 
-    public static void renderPlayerWithIncinerator(AbstractClientPlayerEntity pEntity, MatrixStack pMatrixStack, IRenderTypeBuffer pBuffer, float pPartialTicks) {
+    public static void renderPlayerWithIncinerator(AbstractClientPlayer pEntity, PoseStack pMatrixStack, MultiBufferSource pBuffer, float pPartialTicks) {
         ResourceLocation item = pEntity.getMainHandItem().getItem().getRegistryName();
         if (pEntity.isUsingItem() && item != null && item.equals(ModItems.THE_INCINERATOR.get().getRegistryName())) {
             int i = pEntity.getTicksUsingItem();
             float f2 = pEntity.tickCount + pPartialTicks;
-            float f3 = MathHelper.clamp(i, 1, 60);
+            float f3 = Mth.clamp(i, 1, 60);
             pMatrixStack.pushPose();
-            IVertexBuilder builder = ItemRenderer.getArmorFoilBuffer(pBuffer, CMRenderTypes.getGlowingEffect(FLAME), false, true);
+            VertexConsumer builder = ItemRenderer.getArmorFoilBuffer(pBuffer, CMRenderTypes.getGlowingEffect(FLAME), false, true);
             pMatrixStack.translate(0.0D, 0.001D, 0.0D);
             pMatrixStack.scale(f3 * 0.05F, f3 * 0.05F, f3 * 0.05F);
             pMatrixStack.mulPose(Vector3f.ZP.rotationDegrees(180.0F));
             pMatrixStack.mulPose(Vector3f.YP.rotationDegrees(90.0F + f2));
-            MatrixStack.Entry entry = pMatrixStack.last();
-            Matrix4f matrix4f = entry.pose();
-            Matrix3f matrix3f = entry.normal();
+            PoseStack.Pose pose = pMatrixStack.last();
+            Matrix4f matrix4f = pose.pose();
+            Matrix3f matrix3f = pose.normal();
             drawVertex(matrix4f, matrix3f, builder, -1, -1, 0.0F, 0.0F);
             drawVertex(matrix4f, matrix3f, builder, -1, 1, 0.0F, 1.0F);
             drawVertex(matrix4f, matrix3f, builder, 1, 1, 1.0F, 1.0F);
--- src/main/java/com/teampotato/incinerator/mixin/MixinMinecraftPlayerRenderer.java
+++ src/main/java/com/teampotato/incinerator/mixin/MixinMinecraftPlayerRenderer.java
@@ -1,10 +1,10 @@
 package com.teampotato.incinerator.mixin;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.teampotato.incinerator.Incinerator;
-import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.entity.PlayerRenderer;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerRenderer.class)
 public abstract class MixinMinecraftPlayerRenderer {
-    @Inject(method = "render(Lnet/minecraft/client/entity/player/AbstractClientPlayerEntity;FFLcom/mojang/blaze3d/matrix/MatrixStack;Lnet/minecraft/client/renderer/IRenderTypeBuffer;I)V", at = @At("HEAD"))
-    private void onRender(AbstractClientPlayerEntity pEntity, float pEntityYaw, float pPartialTicks, MatrixStack pMatrixStack, IRenderTypeBuffer pBuffer, int pPackedLight, CallbackInfo ci) {
+    @Inject(method = "render(Lnet/minecraft/client/player/AbstractClientPlayer;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At("HEAD"))
+    private void onRender(AbstractClientPlayer pEntity, float pEntityYaw, float pPartialTicks, PoseStack pMatrixStack, MultiBufferSource pBuffer, int pPackedLight, CallbackInfo ci) {
         Incinerator.renderPlayerWithIncinerator(pEntity, pMatrixStack, pBuffer, pPartialTicks);
     }
 }
