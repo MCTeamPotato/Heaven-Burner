2c2aeacbf7e44dbd86771f47fa79e604be6f292d
Kasualix <670067575@qq.com>
2023-05-05 11:33:27 +0800

    more readable

--- src/main/java/com/teampotato/incinerator/Incinerator.java
+++ src/main/java/com/teampotato/incinerator/Incinerator.java
@@ -31,26 +31,26 @@ public class Incinerator {
                 .endVertex();
     }
 
-    public static void renderPlayerWithIncinerator(AbstractClientPlayerEntity player, MatrixStack matrixStackIn, IRenderTypeBuffer buffer, float pPartialTicks) {
-        ResourceLocation item = player.getMainHandItem().getItem().getRegistryName();
-        if (player.isUsingItem() && item != null && item.equals(ModItems.THE_INCINERATOR.get().getRegistryName())) {
-            int i = player.getTicksUsingItem();
-            float f2 = player.tickCount + pPartialTicks;
+    public static void renderPlayerWithIncinerator(AbstractClientPlayerEntity pEntity, MatrixStack pMatrixStack, IRenderTypeBuffer pBuffer, float pPartialTicks) {
+        ResourceLocation item = pEntity.getMainHandItem().getItem().getRegistryName();
+        if (pEntity.isUsingItem() && item != null && item.equals(ModItems.THE_INCINERATOR.get().getRegistryName())) {
+            int i = pEntity.getTicksUsingItem();
+            float f2 = pEntity.tickCount + pPartialTicks;
             float f3 = MathHelper.clamp(i, 1, 60);
-            matrixStackIn.pushPose();
-            IVertexBuilder builder = ItemRenderer.getArmorFoilBuffer(buffer, CMRenderTypes.getGlowingEffect(SIGIL), false, true);
-            matrixStackIn.translate(0.0D, 0.001D, 0.0D);
-            matrixStackIn.scale(f3 * 0.05F, f3 * 0.05F, f3 * 0.05F);
-            matrixStackIn.mulPose(Vector3f.ZP.rotationDegrees(180.0F));
-            matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(90.0F + f2));
-            MatrixStack.Entry entry = matrixStackIn.last();
+            pMatrixStack.pushPose();
+            IVertexBuilder builder = ItemRenderer.getArmorFoilBuffer(pBuffer, CMRenderTypes.getGlowingEffect(SIGIL), false, true);
+            pMatrixStack.translate(0.0D, 0.001D, 0.0D);
+            pMatrixStack.scale(f3 * 0.05F, f3 * 0.05F, f3 * 0.05F);
+            pMatrixStack.mulPose(Vector3f.ZP.rotationDegrees(180.0F));
+            pMatrixStack.mulPose(Vector3f.YP.rotationDegrees(90.0F + f2));
+            MatrixStack.Entry entry = pMatrixStack.last();
             Matrix4f matrix4f = entry.pose();
             Matrix3f matrix3f = entry.normal();
             drawVertex(matrix4f, matrix3f, builder, -1, -1, 0.0F, 0.0F);
             drawVertex(matrix4f, matrix3f, builder, -1, 1, 0.0F, 1.0F);
             drawVertex(matrix4f, matrix3f, builder, 1, 1, 1.0F, 1.0F);
             drawVertex(matrix4f, matrix3f, builder, 1, -1, 1.0F, 0.0F);
-            matrixStackIn.popPose();
+            pMatrixStack.popPose();
         }
     }
 }
